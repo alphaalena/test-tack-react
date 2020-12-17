c383f71bb16ae3139091b0e5cdc4c2e00104eccf
Alena <kondruh_alena@mail.ru>
2020-12-17 20:12:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Экран магазинов</header>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default App;
